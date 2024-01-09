3cd648854453d4a8f9200a6419d56524131c5645
Massimo Biancalani <massimo.biancalani@ing.com>
2024-01-09 12:33:02 +0100

    Initial commit

---
+++ src/main/java/com/ing/api/contacting/App.java
@@ -0,0 +1,13 @@
+package com.ing.api.contacting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
